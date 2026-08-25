f966d466595dfe5c2e0c936f50b11969bbf6371c
Inactive-Dude <aaronageorge2@gmail.com>
2026-08-25 11:13:00 +0530

    fix: add @NonNull annotations to EventController to match EventService null contracts

--- src/main/java/com/login/communa/Controller/EventController.java
+++ src/main/java/com/login/communa/Controller/EventController.java
@@ -2,6 +2,7 @@ package com.login.communa.Controller;
 
 import com.login.communa.Entity.Event;
 import com.login.communa.Service.EventService;
+import org.springframework.lang.NonNull;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,9 +19,13 @@ public class EventController {
     /** Only admins (ROLE_ADMIN JWT) can post events. */
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/add")
-    public Event addEvent(@RequestBody Event event) { return service.addEvent(event); }
+    public @NonNull Event addEvent(@RequestBody @NonNull Event event) {
+        return service.addEvent(event);
+    }
 
     /** Any authenticated user can read events. */
     @GetMapping("/club/{clubName}")
-    public List<Event> getEvents(@PathVariable String clubName) { return service.getEventsByClub(clubName); }
+    public @NonNull List<Event> getEvents(@PathVariable @NonNull String clubName) {
+        return service.getEventsByClub(clubName);
+    }
 }
